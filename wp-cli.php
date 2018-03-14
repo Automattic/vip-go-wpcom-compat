7b5d4e4b9e47e0c1c25a18b805f52d0dfd28f7de
Josh Betz <j@joshbetz.com>
2018-03-14 17:30:33 -0500

    Use dbDelta to create custom table

--- wp-cli.php
+++ wp-cli.php
@@ -19,18 +19,20 @@ class WPCOM_Compat_Command extends WPCOM_VIP_CLI_Command {
 			WP_CLI::error( 'Specified file does not exist' );
 		}
 
-		global $wpdb;
-
-		$wpdb->query( 'CREATE TABLE IF NOT EXISTS `protected_embeds` (
-			`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-			`embed_id` varchar(64) NOT NULL COMMENT \'unique id md5 ( html )\',
-			`src` varchar(255) NOT NULL COMMENT \'source url\',
-			`embed_group_id` varchar(64) NOT NULL COMMENT \'a unique id for similar widgets\',
-			`html` mediumtext,
-			`time_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-			PRIMARY KEY (`id`),
-			UNIQUE KEY `embed_id` (`embed_id`)
-		) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1' );
+		
+		$sql = 'CREATE TABLE `protected_embeds` ( ' .
+			'`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, ' .
+			'`embed_id` varchar(64) NOT NULL, ' .
+			'`src` varchar(255) NOT NULL, ' .
+			'`embed_group_id` varchar(64) NOT NULL, ' .
+			'`html` mediumtext, ' .
+			'`time_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, ' .
+			'PRIMARY KEY (`id`), ' .
+			'UNIQUE KEY `embed_id` (`embed_id`) ' .
+			') ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 )';
+		
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta( $sql );
 
 		$fd = fopen( $file, 'r' );
 		$rows = fgetcsv( $fd );
