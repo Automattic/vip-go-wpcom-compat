70ff082d6b4ef71f605600e6d1f51559537c7578
Derrick Tennant <derrick.tennant@automattic.com>
2018-05-31 15:24:19 -0400

    Add deprecated functions from WPCOM
    This adds `wpcom_vip_load_wp_rest_api()` as a deprecated function.  This is likely present in some themes, and will fatal on VIP Go since it's a WPCOM specific function.

--- vip-go-wpcom-compat.php
+++ vip-go-wpcom-compat.php
@@ -6,3 +6,14 @@
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once __DIR__ . '/class-wpcom-compat-command.php';
 }
+
+// Deprecated WordPress.com functions.
+
+/**
+ * Loads the built-in WP REST API endpoints in WordPress.com VIP context.
+ *
+ * @deprecated Not applicable since VIP 2.0.0
+ */
+function wpcom_vip_load_wp_rest_api() {
+	_deprecated_function( __FUNCTION__, '2.0.0' );
+}
