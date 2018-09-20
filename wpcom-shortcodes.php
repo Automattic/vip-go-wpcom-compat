43cf38a0fac7bf51681aeb83930d472bb1da592e
Derrick Tennant <emrikol@users.noreply.github.com>
2018-09-20 16:40:17 -0400

    Adding wpcom-shortcodes.php file
    Starting this file with a basic "protected embeds" wrapper.  It's not "protected" but it will at least output the embed code :)

---
+++ wpcom-shortcodes.php
@@ -0,0 +1,34 @@
+<?php
+
+add_shortcode( 'protected-iframe', 'wpcom_compat_protected_iframe_shortcode' );
+
+function wpcom_compat_protected_iframe_shortcode( $attrs ) {
+	$attrs = wp_parse_args( $attrs, array(
+		'id'        => null,
+		'width'     => '',
+		'height'    => '',
+		'scrolling' => '',
+		'class'     => 'wpcom-protected-iframe',
+	) );
+
+	$id    = $args['id'];
+	$embed = wp_cache_get( $id, 'simple-protected-embeds' );
+
+	if ( false === $embed ) {
+		global $wpdb;
+
+		$embed = $wpdb->get_row(
+			$wpdb->prepare( "SELECT html FROM `protected_embeds` WHERE `embed_id` = %s", $id )
+		);
+
+		if ( ! $embed ) {
+			return '<!-- Embed not found -->';
+		}
+
+		$embed = $embed->html;
+
+		wp_cache_set( $id, $embed, 'simple-protected-embeds' );
+	}
+
+	return $embed;
+}
