9784c3e07c37a8c6ec71214c767ff1ab0056a116
Josh Betz <j@joshbetz.com>
2018-10-22 11:38:20 -0500

    Automatically associate imported WP.com accounts
    Associates imported WP.com accounts by email. This ensures Jetpack SSO works
    automatically, as expected.
    
    Fixes #17

--- vip-go-wpcom-compat.php
+++ vip-go-wpcom-compat.php
@@ -9,3 +9,12 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
 require_once __DIR__ . '/wpcom-deprecated-functions.php';
 require_once __DIR__ . '/wpcom-shortcodes.php';
+
+/**
+ * Jetpack SSO: Match WP.com accounts by email
+ *
+ * This ensures user accounts that have been imported from WordPress.com
+ * are still associated with the same WP.com account for the purpose
+ * of Jetpack SSO.
+ */
+add_filter( 'jetpack_sso_match_by_email', '__return_true', 9999 );
