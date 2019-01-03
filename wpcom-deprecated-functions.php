e9e260a1ffe4ef46506663df6185d70c4d920224
Josh Betz <j@joshbetz.com>
2019-01-03 13:35:10 -0600

    Add wpcom_vip_enable_https_canonical
    This function is deprecated on VIP Go, but we need a placeholder to avoid fatals for migrated sites.

--- wpcom-deprecated-functions.php
+++ wpcom-deprecated-functions.php
@@ -9,3 +9,12 @@
 function wpcom_vip_load_wp_rest_api() {
 	_deprecated_function( __FUNCTION__, '2.0.0' );
 }
+
+/**
+ * By default HTTP is forced to be the cannonical version of URLs on WordPress.com.
+ *
+ * @deprecated Not applicable on VIP Go
+ */
+function wpcom_vip_enable_https_canonical() {
+	_deprecated_function( __FUNCTION__, '2.0.0' );
+}
