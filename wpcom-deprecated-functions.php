e56afc509e810ee8b06b768c8edc60199ab23a2b
Josh Betz <j@joshbetz.com>
2019-01-16 09:40:30 -0600

    Deprecated: function require_lib( $slug )
    This  is an internal WordPress.com function that is not applicable on VIP Go. Any shared libraries will need to be copied into VIP Go client repositories as needed.

--- wpcom-deprecated-functions.php
+++ wpcom-deprecated-functions.php
@@ -18,3 +18,16 @@ function wpcom_vip_load_wp_rest_api() {
 function wpcom_vip_enable_https_canonical() {
 	_deprecated_function( __FUNCTION__, '2.0.0' );
 }
+
+/**
+ * Requires WordPress.com internal libraries
+ *
+ * This internal function is used in some WordPress.com themes. These shared libraries
+ * are no longer supported on VIP Go, so they will need to be copied directly into
+ * VIP Go client repositories as needed.
+ *
+ * @deprecated Not supported on VIP Go
+ */
+function require_lib( $slug ) {
+	_deprecated_function( __FUNCTION__, '2.0.0' );
+}
